aa42f97dd478e84b33badb1354ec8608e23d0317
ShahnazMehtiyeva <ShahnazMM@code.local>
2020-02-09 14:27:16 +0400

    modal

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// let close_btn=document.querySelector('.close');
+// let click = document.querySelector('button');
+// let modal=document.querySelector('.modal');
+
+// close_btn.addEventListener('click',function () {
+//     modal.style.display='none';
+// })
+
+
+// click.addEventListener('click',function () {
+//     modal.style.display='block';
+// })
+
+
+// window.addEventListener('click',function () {
+//     if(event.target== modal){
+//         modal.style.display='none';
+//     }
+
+// })
+
+
+//second
+
+// let close_btn = document.querySelector('.close');
+// let images = document.querySelectorAll('.image img');
+// let modal = document.querySelector('.modal');
+// let modalContent=document.querySelector('.modal-content img')
+
+// for (let i = 0; i < images.length; i++) {
+//     images[i].addEventListener('click', function () {
+//          modalContent.src=images[i].src;
+//          modal.style.display = 'block';
+
+//         close_btn.addEventListener('click', function () {
+//             modal.style.display = 'none';
+//         })
+
+//         window.addEventListener('click', function () {
+//             if (event.target == modal) {
+//                 modal.style.display = 'none';
+//             }
+
+//         })
+//     });
+
+// }
+
+
+
